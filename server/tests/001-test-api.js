653d1874a8bf3c20cb6093ef7c42163e08fb3f88
Chad Palmer <chad.n.palmer@gmail.com>
2022-12-13 17:23:00 -0600

    moved base url to env variables

--- server/tests/001-test-api.js
+++ server/tests/001-test-api.js
@@ -1,13 +1,14 @@
+// require('dotenv').config({ path: '../../config/.env' });
 const request = require('supertest');
 
 const testAPI = () => {
-  const baseURL = 'http://localhost:5000';
+  const URL = process.env.BASE_URL;
 
   it('It should pass a simple counting test', () => {
     expect(1 + 1).toEqual(2);
   });
   it('It should pass a simple /GET request', async () => {
-    const res = await request(baseURL).get('/api/sanity');
+    const res = await request(URL).get('/api/sanity');
     expect(res.body.msg).toBe('Passed sanity check!');
   });
 }
