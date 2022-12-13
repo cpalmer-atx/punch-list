7c8499d97007ee236a877d4662969c7cc368423e
Chad Palmer <chad.n.palmer@gmail.com>
2022-12-12 21:17:53 -0600

    configured mongo connection

--- server/db.js
+++ server/db.js
@@ -1,1 +1,23 @@
-// Reserved for MongoDB config
+const mongoose = require('mongoose');
+
+const connectDB = async (URI) => {
+  try {
+    mongoose.set('strictQuery', true);
+    
+    const conn = await mongoose.connect(URI);
+    console.log(`Connection to ${conn.connection.host} database successful.`);
+    conn.connection.on('error', err => {
+      console.error({
+        msg: 'There was an error with the database.',
+        error: err
+      });
+    });
+  } catch (err) {
+    console.error({
+      msg: 'Something went wrong while connecting to the database',
+      error: err
+    });
+  }
+}
+
+module.exports = connectDB;
