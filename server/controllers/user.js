82480a0f6571fab74d05812b31b8ce70c488dff4
Chad Palmer <chad.n.palmer@gmail.com>
2022-12-13 19:43:51 -0600

    search user by email/id, delete/update user

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,7 +8,6 @@ const bcrypt = require('bcryptjs');
 // @route     POST /api/users/
 // @access    Private
 exports.createUser = async (req, res) => {
-  console.log(req.body)
   const errors = validationResult(req);
 
   if (!errors.isEmpty()) {
@@ -63,11 +62,121 @@ exports.getUsers = async (req, res) => {
       count: users.length,
       data: users
     });
-  } catch (err) {
-    console.error(err);
+  } catch (error) {
     res.status(400).json({
       success: false,
-      msg: 'Something went wrong while fetching users.'
+      msg: 'Something went wrong while fetching users.',
+      error: error
+    });
+  }
+}
+
+
+// @desc      Find user by email
+// @route     GET /api/users/search
+// @access    Public
+exports.getUserByEmail = async (req, res) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
+  try {
+    const user = await User.findOne(req.body).select('-_id -password');
+    
+    if (user.email !== req.body.email) {
+      return res.status(400).json({
+        success: false,
+        msg: `No user associated with ${req.body.email} found.`
+      });
+    }
+    res.status(200).json({ success: true, data: user });
+    
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      msg: 'Something went wrong while fetching user.',
+      error: error
+    });
+  }
+}
+
+
+// @desc      Find user by id
+// @route     GET /api/users/:id
+// @access    Private
+exports.getUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id).select('-password');
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        msg: 'No user with provided id found.'
+      });
+    }
+    res.status(200).json({ success: true, data: user });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      msg: 'Something went wrong while fetching user.',
+      error: error
+    });
+  }
+}
+
+
+// @desc      Delete user
+// @route     DELETE /api/users/:id
+// @access    Private
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        msg: 'No user with provided id found.'
+      });
+    }
+    res.status(200).json({ success: true, data: {} });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      msg: 'Something went wrong while deleting user.',
+      error: error
+    });
+  }
+}
+
+
+// @desc      Update user
+// @route     PUT /api/users/:id
+// @access    Private
+exports.updateUser = async (req, res) => {
+  try {
+    const user = await User.findByIdAndUpdate(
+        req.params.id, 
+        req.body,
+        { new: true, runValidators: true }
+      );
+
+    if (!user) {
+      return res.status(400).json({
+        success: false,
+        msg: 'No user with provided id found.'
+      });
+    }
+    res.status(200).json({ success: true, data: user });
+
+  } catch (error) {
+    res.status(400).json({
+      success: false,
+      msg: 'Something went wrong while updating user.',
+      error: error
     });
   }
 }
--- server/routes/user.js
+++ server/routes/user.js
@@ -1,6 +1,8 @@
 const { check } = require('express-validator');
 const express = require('express');
-const { getUsers, createUser } = require('../controllers/user');
+
+const { getUsers, getUser, getUserByEmail, 
+        createUser, deleteUser, updateUser } = require('../controllers/user');
 
 const router = express.Router();
 
@@ -12,5 +14,17 @@ router
     check('email', 'Valid email required').isEmail(),
     check('password', 'Password with more than 4 characters required').isLength({ min: 5 })
   ], createUser);
+
+router
+  .route('/search')
+  .get([
+    check('email', 'Valid email required').isEmail()
+  ], getUserByEmail);
+
+router
+  .route('/:id')
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
 module.exports = router;
